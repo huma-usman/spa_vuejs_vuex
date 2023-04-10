20664626cbf966129ef3a892304713c4fb81391d
Huma Usman <humaakber24@gmail.com>
2023-04-10 20:33:45 +0500

    updating table styling, sorting and details showing

--- src/components/DataTable.vue
+++ src/components/DataTable.vue
@@ -1,25 +1,41 @@
 <template>
   <div>
-    <b-table id="my-table" striped hover :items="data" :per-page="0" :fields="fields">
+    <div>
+      Sorting By: <b>{{ sortBy }}</b>, Sort Direction:
+      <b>{{ sortDesc ? 'Descending' : 'Ascending' }}</b>
+    </div>
+    <b-table id="my-table"
+      :bordered="bordered"
+      :head-variant="headVariant"
+      :items="data"
+      :per-page="0"
+      :fields="fields"
+      :sort-by.sync="sortBy"
+      :sort-desc.sync="sortDesc"
+      :class="my-table"
+      hover
+      caption-top
+      responsive
+      sort-icon-left>
+      <template #table-caption>Data retrieved from an API endpoint</template>
       <template #cell(view)="row">
-        <b-button @click="row.toggleDetails(); fetchSelectedData(row.item.id) ">
-          Details
+        <b-button @click=" fetchSelectedData(row.item.id); row.toggleDetails() ">
+          {{ row.detailsShowing ? 'Hide' : 'Show'}} Details
         </b-button>
       </template>
-
       <template #row-details="row">
         <b-card>
-          <b-row class="mb-1">
-            <b-col sm="3" class="text-sm-right"><b>Name:</b></b-col>
-            <b-col>{{ row.item.name }}</b-col>
+          <b-row  class="mb-1">
+            <b-col sm="2" class="text-sm-right"><b>{{ row.item.id != selectedData.id ? '': 'Name:' }}</b></b-col>
+            <b-col>{{ row.item.id != selectedData.id ? 'Hit the Hide Details button and Show Details again': row.item.name }}</b-col>
+            </b-row>
+          <b-row  class="mb-1">
+            <b-col sm="2" class="text-sm-right"><b>{{ row.item.id != selectedData.id ? '': 'Description:' }}</b></b-col>
+            <b-col>{{ row.item.id != selectedData.id ? '':selectedData.description }}</b-col>
           </b-row>
           <b-row class="mb-1">
-            <b-col sm="3" class="text-sm-right"><b>Description:</b></b-col>
-            <b-col>{{ selectedData.description }}</b-col>
-          </b-row>
-          <b-row class="mb-1">
-            <b-col sm="3" class="text-sm-right"><b>Created at:</b></b-col>
-            <b-col>{{ selectedData.date }}</b-col>
+            <b-col sm="2" class="text-sm-right"><b>{{ row.item.id != selectedData.id ? '': 'Created at:' }}</b></b-col>
+            <b-col>{{ row.item.id != selectedData.id ? '':selectedData.date }}</b-col>
           </b-row>
         </b-card>
       </template>
@@ -32,7 +48,15 @@
 export default {
   name: 'DataTable',
   data: () => ({
-    fields: ['id', 'value', 'name', 'view'],
+    sortBy: 'value',
+    sortDesc: false,
+    fields: [
+      { key: 'id', sortable: true },
+      { key: 'value', sortable: true },
+      { key: 'name', sortable: true },
+      { key: 'view', sortable: false }
+    ],
+   headVariant:'info',
   }),
   computed: {
     data() {
@@ -49,33 +73,12 @@ export default {
   }
 }
 </script>
-
 <style>
-#app {
-  font-family: 'Avenir', Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
-  margin-top: 60px;
-}
-
-h1,
-h2 {
-  font-weight: normal;
-}
-
-ul {
-  list-style-type: none;
-  padding: 0;
-}
-
-li {
-  display: inline-block;
-  margin: 0,10
-}
-
-a {
-  color: #b9428b;
-}
+  /* Add !important to override other styles */
+  .thead-info {
+    background-color: rgba(17, 183, 212, 0.575)!important;
+  }
+  .my-table {
+    background-color: rgba(158, 228, 255, 0.87)!important; /* set your desired background color here */
+  }
 </style>
